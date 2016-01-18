f201b78d49cdc778c98dddfacf7da5d6c5d960aa
OneOne <mika.23.joo@gmail.com>
2016-01-19 00:43:54 +0100

    MJ: second drop - pizza jumping !

---
+++ Play2Dash/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float MaxMoveSpeed = 10.0f;
+	public float JumpForce = 700.0f;
+	public Transform GroundCheck;
+	public LayerMask GroundLayer;
+
+	public float GroundRaduis = 0.2f;
+
+
+	// Use this for initialization
+	void Start () {
+		_rigidBody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	private bool _facingRight = true;
+	private bool _onGround =  false;
+
+	private Rigidbody2D _rigidBody2D;
+
+	void Update() {
+		if (_onGround && Input.GetButtonDown ("Jump")) {
+			_onGround = false;
+			_rigidBody2D.AddForce (new Vector2 (0, JumpForce));
+		}
+	}
+
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+
+		_onGround = Physics2D.OverlapCircle (GroundCheck.position, GroundRaduis, GroundLayer);
+
+				
+		_rigidBody2D.velocity = new Vector2 (move * MaxMoveSpeed, _rigidBody2D.velocity.y);
+
+
+		if (move > 0 && !_facingRight)
+			Flip ();
+		else if (move < 0 && _facingRight)
+			Flip ();
+	}
+
+	void Flip() {
+		_facingRight = !_facingRight;
+		Vector3 lScale = transform.localScale;
+		lScale.x *= -1;
+		transform.localScale = lScale;
+		//transform.localScale.x *= -1;
+	}
+}
---
+++ Play2Dash/Assets/Scripts/TileManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class TileManager : MonoBehaviour {
+
+	//public TextAsset Map;
+	public Texture2D Map;
+	public GameObject TileBlock;
+	public double TileSize;
+
+	// Use this for initialization
+	void Start () {
+		LoadMap (Map);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private List<GameObject> _tiles = new List<GameObject>();
+
+	void LoadMap(Texture2D iMap)
+	{
+
+		//Texture2D tex = new Texture2D(0, 0);
+		//tex.LoadImage(iMap.bytes);
+
+		for (int i = 0; i < iMap.width; i++)
+		{
+			for (int j = 0; j < iMap.height; j++)
+			{
+				Color pixel = iMap.GetPixel(i,j);
+
+				if (pixel == Color.black)
+				{
+					GameObject tile = (GameObject)Instantiate (TileBlock, new Vector3 ((float)(i * TileSize), (float)(j * TileSize), this.transform.position.z), new Quaternion ());
+					tile.transform.parent = this.transform;
+					_tiles.Add(tile);
+
+				}
+			}
+		} 
+	}
+}
+
+
