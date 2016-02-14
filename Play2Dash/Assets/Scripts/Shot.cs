06a9907e469258e345eaaf884db79a172c21dcef
Macdup <dupont.marcantoine.isart@gmail.com>
2016-02-14 02:36:50 +0100

    MegamanSize + levelTest size + HUD prefab + Shot prefab

---
+++ Play2Dash/Assets/Scripts/Shot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+
+	public Vector3 moveVector;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(moveVector * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("test");
+	}
+}
